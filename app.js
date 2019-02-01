3294fc8ed51b8a172cb5b757463174a2795fefb0
Haley <haleymlewis@gmail.com>
2019-01-31 19:18:12 -0800

    First major save

--- app.js
+++ app.js
@@ -4,25 +4,27 @@
 
 var pets = prompt('How many pets do I own?');
 console.log('Number of pets, ', pets);
-if(pets === '2'){
-  alert('Correct, I have 2 pets!');
-}else{
-  alert('No, I have 2 pets!')
+while(pets !== '2'){
+  alert('Nope, guess again!');
+  var pets = prompt('How many pets do I ACTUALLY have?');
 };
+alert('Correct, I have 2 pets!');
 
 //Question 2
 
 var years = prompt('How many years have I lived in Washington?');
 console.log('Years lived in WA: ', years);
-if(years === '28'){
-  alert('Correct, I have lived in WA for ' + years + ' Years!');
-}else{
-  alert('No, I have lived in WA for 28 Years!');
+while(years !== '28'){
+  alert('Nope, guess again!');
+  var years = prompt('How many years have I ACTUALLY lived in Washington?');
 };
+alert('Correct, I have lived in WA for 28 years!');
 
 // //Question 3
-// var college = prompt('What year did I graduate from college?');
-
-// console.log('Year graduated, ' college);
-
-// alert('Year graduated, ' college);
+var college = prompt('What year did I graduate from college?');
+console.log('Year graduated, ' college);
+while(college !== '2011'){
+  alert('Nope, guess again! Probably earlier than you\'d think...');
+  var years = prompt('What year did I graduate from college?');
+}
+alert('Yep, I graduated in 2011. At the ripe old age of 20. When I was young...');
