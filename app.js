46cba6364c2daddb7086f4c1bb5a9d5017a5e86a
Haley <haleymlewis@gmail.com>
2019-01-31 18:46:37 -0800

    fixed

---
+++ app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+//Question 1
+
+var pets = prompt('How many pets do I own?');
+console.log('Number of pets, ', pets);
+if(pets === '2'){
+  alert('Correct, I have 2 pets!');
+}else{
+  alert('No, I have 2 pets!')
+};
+
+//Question 2
+
+var years = prompt('How many years have I lived in Washington?');
+console.log('Years lived in WA: ', years);
+if(years === '28'){
+  alert('Correct, I have lived in WA for ' + years + ' Years!');
+}else{
+  alert('No, I have lived in WA for 28 Years!');
+};
+
+// //Question 3
+// var college = prompt('What year did I graduate from college?');
+
+// console.log('Year graduated, ' college);
+
+// alert('Year graduated, ' college);
