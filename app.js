ba55d9176813755217bad65872bd07e4f1eee098
Haley <haleymlewis@gmail.com>
2019-02-02 15:52:36 -0800

    saving after replacing loops

--- app.js
+++ app.js
@@ -24,17 +24,19 @@ alert('Correct, I have lived in WA for 28 years!');
 
 var college = prompt('What year did I graduate from college?');
 console.log('Haley graduated from college in ' + college);
-while(college !== '2011'){
-  alert('Nope, guess again! Probably earlier than you\'d think...');
-  var college = prompt('What year did I graduate from college?');
-  if(college === '2011'){
-    alert('Yep, I graduated in 2011. At the ripe old age of 20.');
+for (i = 0; i < 4; i++) {
+  if (college === '2011') {
+    alert('Yep, I graduated in 2011.');
+    break;
+  } else {
+    college = prompt('Nope, guess again! Probably ealier than you\'d think...');
   }
-} alert('Yep, I graduated in 2011');
+}
 
-// Question 4
 
-var milk = prompt('Am I Lacotse Intolerant?')
+// // Question 4
+
+var milk = prompt('Am I Lactose Intolerant?')
 console.log(milk + ' Haley is lactose intolerant.');
 milk.toLowerCase();
 if(milk === 'yes') {
@@ -48,26 +50,49 @@ if(milk === 'yes') {
 var femme = prompt('Am I am feminist?')
 femme.toLowerCase();
 console.log(femme +' Haley is a proud feminist');
-if(femme === yes) {
+if(femme === 'yes') {
   alert('Yep, I sure am!');
 }else{
   alert('Nope, I sure am! It\'s not a dirty word!');
 }
 
 // Question 6
-var badGuess = 0;
 var imdb = prompt('How many credits do I have on my IMdB page? Please use numerical input!');
-while (badGuess !== 6) {
+console.log(imdb);
+for (var i = 0; i < 4; i++) {
+  console.log('for loop starts');
+  if (imdb === '6') {
+    alert('Correct, I have 6 IMdB credits!');
+    console.log('IMdB logged as 6:' + imdb);
+    break;
+  }
+  else if (imdb >= 7) {
+    alert('WHOA, no way! Lower!');
+    console.log('IMdB logged as >=7: ' + imdb);
+  }
+  else {
+    alert('No, more! Come on, give me SOME credit!');
+    console.log('IMdB < 6: ' + imdb);
+  }
+  console.log('for loop ends');
   imdb = prompt('Nope, guess again!');
-  if(imdb <= 5){
-    imdb = prompt('Nope, too low! Give me SOME credit...');
-  }
-  else if(imdb >= 7){
-    imdb = prompt('WHOA, no way! Lower!');
-  } 
-  else if(imdb === 6){
-    alert('Correct, I have 6 IMdB Credits.');
-    badGuess = 6
-  }
-  badGuess++
 }
+
+
+// while (badGuess !== 6){
+//   imdb = prompt('Nope, guess again!');
+//   if(imdb === 6){
+//     alert('Correct, I have 6 IMdB credits!');
+//     console.log('IMdB logged as 6:' + imdb)
+//   }
+//   else if(imdb >= 7){
+//     alert('WHOA, no way! Lower!');
+//     badGuess++
+//     console.log('IMdB logged as >=7: ' + imdb)
+//   } 
+//   else {
+//     alert('No, more! Come on, give me SOME credit!');
+//     badGuess = 6
+//     console.log('IMdB < 6: ' + imdb);
+//   }
+// }
